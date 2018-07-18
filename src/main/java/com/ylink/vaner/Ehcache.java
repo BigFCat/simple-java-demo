9612faf1e2c95549332c9b36b1941175cb323efa
weixiaojie <18565765503@163.com>
2018-07-18 23:22:54 +0800

    1

--- src/main/java/com/ylink/vaner/Ehcache.java
+++ src/main/java/com/ylink/vaner/Ehcache.java
@@ -5,24 +5,14 @@ import net.sf.ehcache.CacheManager;
 import net.sf.ehcache.Element;
 
 public class Ehcache {
-	 // Create a cache manager
-    final CacheManager cacheManager = new CacheManager();
-
-    // create the cache called "hello-world"
-    final Cache cache = cacheManager.getCache("hello-world");
-
-    // create a key to map the data to
-    final String key = "greeting";
-
-    // Create a data element
-    final Element putGreeting = new Element(key, "Hello, World!");
-
-    // Put the element into the data store
-//    cache.put(putGreeting);
-
-    // Retrieve the data element
-    final Element getGreeting = cache.get(key);
-
-    // Print the value
-//    System.out.println(getGreeting.getObjectValue());
+	public static void main(String[] args) {
+		final CacheManager cacheManager = new CacheManager(Ehcache.class.getClassLoader().getResourceAsStream("ehcache-auto.xml"));
+	    final Cache cache = cacheManager.getCache("users");
+	    final String key = "greeting";
+	    final Element putGreeting = new Element(key, "Hello, World!");
+	    cache.put(putGreeting);
+	    final Element getGreeting = cache.get(key);
+	    System.out.println(getGreeting.getObjectValue());
+	}
+    
 }
